024916887a2728f00c563d09a4f718f6818c741e
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-10-06 18:15:14 +0300

    Add Dbcontext file.

---
+++ HotelInfo.API/DbContexts/HotelInfoContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelInfo.API.Entites;
+
+namespace HotelInfo.API.DbContexts
+{
+    public class HotelInfoContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+
+        public HotelInfoContext(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration["ConnectionStrings:SSMSConnectionString"]);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
