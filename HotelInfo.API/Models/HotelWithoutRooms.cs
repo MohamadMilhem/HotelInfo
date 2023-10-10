36462992953201ae24f47268c045ad3ab0ad074d
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-10-10 11:24:35 +0300

    Fix object cycle.

--- HotelInfo.API/Models/HotelWithoutRooms.cs
+++ HotelInfo.API/Models/HotelWithoutRooms.cs
@@ -8,8 +8,6 @@ namespace HotelInfo.API.Models
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public City? City { get; set; }
-        public int CityId { get; set; }
         public string Description { get; set; } = null!;
         public HotelType HotelType { get; set; }
         public int StarRating { get; set; }
