12254b75de323b6d296dfc8fc0eb7271b78b0642
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-10-29 16:36:02 +0200

    Add MainController file.

---
+++ HotelInfo.API/Controllers/MainController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using HotelInfo.API.Entites;
+using HotelInfo.API.Models;
+using HotelInfo.API.Services;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace HotelInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/main")]
+    public class MainController : ControllerBase
+    {
+        private readonly IHotelInfoRepository _hotelInfoRepository;
+        private readonly IMapper _mapper;
+        const int maxPageSize = 20;
+
+        public MainController(IHotelInfoRepository hotelInfoRepository, IMapper mapper)
+        {
+            _hotelInfoRepository = hotelInfoRepository ?? throw new ArgumentNullException(nameof(hotelInfoRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        
+        [HttpGet("search")]
+        public async Task<IActionResult> GetSearchResults(
+            string? name, string? searchQuery, int pageSize = 10, int pageNumber = 1)
+        {
+            var (cities, paginationMetaDataForCities) = await _hotelInfoRepository.GetCitiesAsync(name, searchQuery, pageSize, pageNumber);
+            var (hotels, paginationMetaDataForHotels) = await _hotelInfoRepository.GetHotelsAsync(name, searchQuery, pageSize, pageNumber);
+
+            Response.Headers.Add("Cities-Pagination", JsonSerializer.Serialize(paginationMetaDataForCities));
+            Response.Headers.Add("Hotels-Pagination", JsonSerializer.Serialize(paginationMetaDataForHotels));
+
+            var citiesToReturn = _mapper.Map<IEnumerable<CitySearchResult>>(cities);
+            var hotelsToReturn = _mapper.Map<IEnumerable<HotelSearchResult>>(hotels);
+            
+            var results = new
+            {
+                Cities = citiesToReturn,
+                Hotels = hotelsToReturn
+            };
+            return Ok(results);
+        }
+
+        [HttpGet("RecentlyVisited")]
+        public async Task<IActionResult> GetRecentlyVisitedHotels()
+        {
+            var (hotels, paginationMetaData) = await _hotelInfoRepository.GetHotelsAsync(null, null, 10, 1);
+            var hotelsToReturn = _mapper.Map<IEnumerable<HotelSearchResult>>(hotels.Take(5));
+            return Ok(hotelsToReturn);
+        }
+        
+        
+    }
+}
