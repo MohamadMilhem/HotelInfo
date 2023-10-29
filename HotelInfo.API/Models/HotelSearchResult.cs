0290fe37d88ec605d52fa042004b7bdc31e45978
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-10-29 16:32:18 +0200

    Add Hotel and City Models for search results.

---
+++ HotelInfo.API/Models/HotelSearchResult.cs
@@ -0,0 +1,16 @@
+﻿using HotelInfo.API.Entites;
+
+namespace HotelInfo.API.Models;
+
+public class HotelSearchResult
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? CityName { get; set; }
+    public string Description { get; set; } = null!;
+    public HotelType HotelType { get; set; }
+    public int StarRating { get; set; }
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
+    public PhotoDto ThumbnailImage { get; set; }
+}
