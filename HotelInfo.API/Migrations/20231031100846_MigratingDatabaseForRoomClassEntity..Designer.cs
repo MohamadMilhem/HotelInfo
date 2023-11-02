8084de05e19261c37bab3dbd9f3f1d84cb722617
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-11-02 13:03:03 +0200

    Apply Migration to create RoomClass Table.

--- HotelInfo.API/Migrations/20231031100846_MigratingDatabaseForRoomClassEntity..Designer.cs
+++ HotelInfo.API/Migrations/20231031100846_MigratingDatabaseForRoomClassEntity..Designer.cs
@@ -4,6 +4,7 @@ using HotelInfo.API.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelInfo.API.Migrations
 {
     [DbContext(typeof(HotelInfoContext))]
-    partial class HotelInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20231031100846_MigratingDatabaseForRoomClassEntity.")]
+    partial class MigratingDatabaseForRoomClassEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
